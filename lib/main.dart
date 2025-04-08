b51033a74e34c68125ac1ba87b990b8d08320b42
Chauhan Satvik <chauhansatvik07@gmail.com>
2025-04-08 15:52:50 +0530

    Fixed Minor Bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tip Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const TipCalculator(),
     );
   }
@@ -28,27 +26,24 @@ class TipCalculator extends StatefulWidget {
 }
 
 class _TipCalculatorState extends State<TipCalculator> {
-    String result = '';
+  String result = '';
 
-    final amountController = TextEditingController();
-    final tipController = TextEditingController();
+  final amountController = TextEditingController();
+  final tipController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
     final border = OutlineInputBorder(
       borderSide: const BorderSide(color: Colors.black, width: 2),
-      borderRadius: BorderRadius.circular(14)
+      borderRadius: BorderRadius.circular(14),
     );
-
-
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: const Text('Tip Calculator'),
         centerTitle: true,
-        backgroundColor: const Color.fromRGBO(96, 125, 139, 1)  ,
+        backgroundColor: const Color.fromRGBO(96, 125, 139, 1),
       ),
       body: Center(
         child: Column(
@@ -59,15 +54,13 @@ class _TipCalculatorState extends State<TipCalculator> {
             Padding(
               padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
               child: TextField(
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
                 controller: amountController,
                 decoration: InputDecoration(
                   hintText: 'Enter Your Bill Amount (₹)',
                   filled: true,
                   fillColor: const Color.fromARGB(255, 118, 154, 172),
-                  hintStyle: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white
-                  ),
+                  hintStyle: const TextStyle(fontSize: 20, color: Colors.white),
                   focusedBorder: border,
                   enabledBorder: border,
                 ),
@@ -77,28 +70,26 @@ class _TipCalculatorState extends State<TipCalculator> {
             Padding(
               padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
               child: TextField(
+                keyboardType: TextInputType.numberWithOptions(decimal: false),
                 controller: tipController,
                 decoration: InputDecoration(
                   hintText: 'Enter The Tip (%)',
                   filled: true,
                   fillColor: const Color.fromARGB(255, 118, 154, 172),
-                  hintStyle: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white
-                  ),
+                  hintStyle: const TextStyle(fontSize: 20, color: Colors.white),
                   focusedBorder: border,
                   enabledBorder: border,
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10 , top: 3),
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 3),
               child: ElevatedButton(
                 onPressed: () {
                   final amountText = amountController.text;
                   final tipText = tipController.text;
 
-  // Safety check: Are both fields filled?
+                  // Safety check: Are both fields filled?
                   if (amountText.isEmpty || tipText.isEmpty) {
                     setState(() {
                       result = 'Please enter both fields!';
@@ -107,15 +98,15 @@ class _TipCalculatorState extends State<TipCalculator> {
                   }
 
                   final amount = double.tryParse(amountText);
-                  final tipPercent = double.tryParse(tipText);
+                  final tipPercent = int.tryParse(tipText);
 
-  // Check if parsing worked
+                  // Check if parsing worked
                   if (amount == null || tipPercent == null) {
-                   setState(() {
+                    setState(() {
                       result = 'Invalid input!';
                     });
                     return;
-                 }
+                  }
 
                   final tipAmount = amount * (tipPercent / 100);
                   final totalAmount = amount + tipAmount;
@@ -123,19 +114,20 @@ class _TipCalculatorState extends State<TipCalculator> {
                   setState(() {
                     result = 'Total: ₹${totalAmount.toStringAsFixed(2)}';
                   });
+                  Future.delayed(Duration(seconds: 5), () {
+                    amountController.clear();
+                    tipController.clear();
+                  });
                 },
                 style: ElevatedButton.styleFrom(
                   elevation: 15,
-                  textStyle: TextStyle(
-                    color: Colors.white
-                  ),
+                  textStyle: TextStyle(color: Colors.white),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(14),
                   ),
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
                   minimumSize: const Size(double.infinity, 50),
-                  
                 ),
                 child: const Text('Calculate'),
               ),
